b35a2f2ab9bd3f8381d6d3804d5e8d0373142121
parthsingh238 <parthh.2007@gmail.com>
2026-02-28 21:49:44 +0530

    Add files via upload

---
+++ DSA-100days/day28.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if (n <= 0) return 0;
+
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    for (int i = 0; i < n; i++) {
+        int value;
+        scanf("%d", &value);
+
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = value;
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    // Make it circular
+    temp->next = head;
+
+    // Traversal
+    struct Node* current = head;
+
+    if (head != NULL) {
+        do {
+            printf("%d ", current->data);
+            current = current->next;
+        } while (current != head);
+    }
+
+    return 0;
+}
